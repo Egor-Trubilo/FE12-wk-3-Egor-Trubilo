7985ee13202ef60f9c37e6d2a0bd71faf8f8b63b
Egor_Trubilo <trubilooficial@gmail.com>
2021-09-16 19:04:18 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import from './styles/index.css'
-
-import summ from './modules/summ'
+import "./styles/style.css";
+import "./styles/index.scss";
+import summ from "./modules/summ";
 
 console.log('Hello World!, summ( a: 12, b: 2)');
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
 const path = require('path');
-
-// export const entry = './src/index.js';
-// export const output = {
-//     path: resolve(__dirname, 'build'),
-//     filename: '[name].[contenthash].js',
-//     clean: true,
-// };
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         path: resolve(__dirname, 'build'),
         filename: '[name].[contenthash].js',
         clean: true,
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html'
+    })], 
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    },
+    ],
+  },
 }
